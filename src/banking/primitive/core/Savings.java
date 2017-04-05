9a3cf4a03257db033026bc3b045d53e7f72fbbe1
jstokke25 <jstokke25@yahoo.com>
2017-04-05 04:15:33 -0700

    Changed bal < 0 before OVERDRAWN in Savings.withdraw

--- src/banking/primitive/core/Savings.java
+++ src/banking/primitive/core/Savings.java
@@ -13,7 +13,13 @@ public class Savings extends Account {
 	}
 
 	/**
-	 * A deposit comes with a fee of 50 cents per deposit
+	  Method:  deposit
+	  Inputs:  @amount
+	  Returns: boolean
+
+	  Description:  If Account not closed and deposit amt > 0, then deposit occurs
+	                A deposit comes with a fee of 50 cents per deposit
+	 * 
 	 */
 	public boolean deposit(float amount) {
 		if (getState() != State.CLOSED && amount > 0.0f) {
@@ -27,7 +33,11 @@ public class Savings extends Account {
 	}
 
 	/**
-	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
+	   Method:  withdraw
+	   Inputs:  @amount
+	   Returns: boolean
+
+	  Description:A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
 	 * An account whose balance dips below 0 is in an OVERDRAWN state
 	 */
 	public boolean withdraw(float amount) {
@@ -37,7 +47,7 @@ public class Savings extends Account {
 			if (numWithdraws > 3)
 				balance = balance - 1.0f;
 			// KG BVA: should be < 0
-			if (balance <= 0.0f) {
+			if (balance < 0.0f) {
 				setState(State.OVERDRAWN);
 			}
 			return true;
